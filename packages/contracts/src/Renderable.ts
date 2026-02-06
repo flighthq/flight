956f761921be927bdbdc9bcfe11e8c03b82722f1
jgranick <jgranick@users.noreply.github.com>
2026-02-05 21:16:11 -0800

    contracts: Update Renderable to use Matrix3

--- packages/contracts/src/Renderable.ts
+++ packages/contracts/src/Renderable.ts
@@ -1,4 +1,4 @@
-import type { BitmapFilter, BlendMode, Matrix2D, Rectangle, Shader } from '@flighthq/types';
+import type { BitmapFilter, BlendMode, Matrix3, Rectangle, Shader } from '@flighthq/types';
 
 import { RenderableSymbols as R } from './RenderableSymbols.js';
 
@@ -7,13 +7,13 @@ export interface Renderable {
   readonly [R.blendMode]: BlendMode;
   readonly [R.bounds]: Rectangle;
   readonly [R.cacheAsBitmap]: boolean;
-  readonly [R.cacheAsBitmapMatrix]: Matrix2D | null;
+  readonly [R.cacheAsBitmapMatrix]: Matrix3 | null;
   readonly [R.children]: Renderable[] | null;
   readonly [R.filters]: BitmapFilter[] | null;
   readonly [R.height]: number;
   readonly [R.localBounds]: Rectangle;
   readonly [R.localBoundsID]: number;
-  readonly [R.localTransform]: Matrix2D;
+  readonly [R.localTransform]: Matrix3;
   readonly [R.localTransformID]: number;
   readonly [R.mask]: Renderable | null;
   readonly [R.maskedObject]: Renderable | null;
@@ -31,7 +31,7 @@ export interface Renderable {
   readonly [R.shader]: Shader | null;
   readonly [R.width]: number;
   readonly [R.worldBounds]: Rectangle;
-  readonly [R.worldTransform]: Matrix2D;
+  readonly [R.worldTransform]: Matrix3;
   readonly [R.worldTransformID]: number;
   readonly [R.visible]: boolean;
   readonly [R.x]: number;
