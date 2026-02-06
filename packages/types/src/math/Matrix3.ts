075bad7405700f5cf02f68c59ef0ac29e05f146a
jgranick <jgranick@users.noreply.github.com>
2026-02-05 21:09:17 -0800

    types: Remove Affine2D for now, Matrix3 type simpler

--- packages/types/src/math/Matrix3.ts
+++ packages/types/src/math/Matrix3.ts
@@ -1,11 +1,3 @@
 export default interface Matrix3 {
-  m00: number;
-  m01: number;
-  m02: number;
-  m10: number;
-  m11: number;
-  m12: number;
-  m20: number;
-  m21: number;
-  m22: number;
+  readonly m: Float32Array;
 }
--- packages/types/src/math/index.ts
+++ packages/types/src/math/index.ts
@@ -1,4 +1,3 @@
-export { default as Affine2D } from './Affine2D.js';
 export { default as Matrix3 } from './Matrix3.js';
 export { default as Rectangle } from './Rectangle.js';
 export { default as Vector2 } from './Vector2.js';
