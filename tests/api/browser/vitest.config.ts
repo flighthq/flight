aa519778782c522ef8e1e8bc499c3b10caca8ecc
jgranick <jgranick@users.noreply.github.com>
2026-02-26 18:42:29 -0800

    tests: Remove special alias paths in API tests

--- tests/api/browser/vitest.config.ts
+++ tests/api/browser/vitest.config.ts
@@ -11,17 +11,5 @@ export default mergeConfig(
       environment: 'jsdom',
       include: ['**/*.test.ts'],
     },
-    resolve: {
-      alias: {
-        '@flight/materials': path.resolve(__dirname, '../../../packages/materials/dist/index.js'),
-        '@flight/flight': path.resolve(__dirname, '../../../packages/flight/dist/index.js'),
-        '@flight/interaction': path.resolve(__dirname, '../../../packages/interaction/dist/index.js'),
-        '@flight/geom': path.resolve(__dirname, '../../../packages/geom/dist/index.js'),
-        '@flight/render-core': path.resolve(__dirname, '../../packages/render-core/dist/index.js'),
-        '@flight/render-canvas': path.resolve(__dirname, '../../packages/render-canvas/dist/index.js'),
-        '@flight/stage': path.resolve(__dirname, '../../../packages/stage/dist/index.js'),
-        '@flight/types': path.resolve(__dirname, '../../../packages/types/dist/index.js'),
-      },
-    },
   }),
 );
--- tests/api/vitest.config.ts
+++ tests/api/vitest.config.ts
@@ -11,20 +11,5 @@ export default mergeConfig(
       environment: 'node',
       include: ['**/*.test.ts', '!browser/**/*.test.ts'],
     },
-    resolve: {
-      alias: {
-        '@flight/assets': path.resolve(__dirname, '../../packages/assets/dist/index.js'),
-        '@flight/flight': path.resolve(__dirname, '../../packages/flight/dist/index.js'),
-        '@flight/geom': path.resolve(__dirname, '../../packages/geom/dist/index.js'),
-        '@flight/interaction': path.resolve(__dirname, '../../packages/interaction/dist/index.js'),
-        '@flight/materials': path.resolve(__dirname, '../../packages/materials/dist/index.js'),
-        '@flight/render-canvas': path.resolve(__dirname, '../../packages/render-canvas/dist/index.js'),
-        '@flight/render-core': path.resolve(__dirname, '../../packages/render-core/dist/index.js'),
-        '@flight/stage': path.resolve(__dirname, '../../packages/stage/dist/index.js'),
-        '@flight/timeline': path.resolve(__dirname, '../../packages/timeline/dist/index.js'),
-        '@flight/types': path.resolve(__dirname, '../../packages/types/dist/index.js'),
-        '@flight/world': path.resolve(__dirname, '../../packages/world/dist/index.js'),
-      },
-    },
   }),
 );
