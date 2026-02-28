b0c522d7bb9ec8d864451261702d3e2577bb45d0
jgranick <jgranick@users.noreply.github.com>
2026-02-27 17:38:49 -0800

    render-canvas: Add tests

---
+++ packages/render-canvas/src/transform.test.ts
@@ -0,0 +1,42 @@
+import { matrix3x2 } from '@flighthq/geom';
+import type { CanvasRendererState, Matrix3x2 } from '@flighthq/types';
+
+import { createCanvasRendererState } from './createCanvasRendererState';
+import { setTransform } from './transform';
+
+describe('setTransform', () => {
+  let canvas: HTMLCanvasElement;
+  let state: CanvasRendererState;
+  let transform: Matrix3x2;
+
+  beforeEach(() => {
+    canvas = document.createElement('canvas');
+    state = createCanvasRendererState(canvas);
+    transform = matrix3x2.create(1, 2, 3, 4, 5.6789, 9.1011);
+  });
+
+  it('should call setTransform with correct values when roundPixels is false', () => {
+    setTransform(state, state.context, transform);
+    expect(state.context.setTransform).toHaveBeenCalledWith(
+      transform.a,
+      transform.b,
+      transform.c,
+      transform.d,
+      transform.tx,
+      transform.ty,
+    );
+  });
+
+  it('should call setTransform with frounded tx and ty when roundPixels is true', () => {
+    state.roundPixels = true;
+    setTransform(state, state.context, transform);
+    expect(state.context.setTransform).toHaveBeenCalledWith(
+      transform.a,
+      transform.b,
+      transform.c,
+      transform.d,
+      Math.fround(transform.tx),
+      Math.fround(transform.ty),
+    );
+  });
+});
