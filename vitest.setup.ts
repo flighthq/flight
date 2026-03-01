9ab869d3bd192886b4520df911bb3e28943f50e5
jgranick <jgranick@users.noreply.github.com>
2026-02-28 19:27:14 -0800

    tests: Quiet warning

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,5 +1,6 @@
 if (typeof window !== 'undefined') {
   // jsdom / browser environment
+  // @ts-expect-error: quiet warning about types
   import('@testing-library/jest-dom');
   import('vitest-webgl-canvas-mock');
 } else {
