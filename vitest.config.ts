4ced62a8e4b468e20a3b008fe0e7527de8d7e7fb
jgranick <jgranick@users.noreply.github.com>
2026-03-05 13:59:40 -0800

    tests: Almost working smart top-level config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,5 @@
+import { resolve } from 'path';
+import type { TestProjectConfiguration } from 'vitest/config';
 import { defineConfig, mergeConfig } from 'vitest/config';
 
 import baseConfig from './vitest.config.base.js';
@@ -7,21 +9,41 @@ export default mergeConfig(
   defineConfig({
     test: {
       projects: [
-        'packages/animation/timeline',
-        'packages/assets',
-        'packages/flight',
-        'packages/geometry',
-        'packages/interaction',
-        'packages/materials',
-        'packages/render/canvas',
-        'packages/render/core',
-        'packages/scene/graph/stage',
-        'packages/scene/graph/world',
-        'packages/types',
-        'tests/api',
-        'tests/api/browser',
-        'tests/integration',
+        config('packages/animation/timeline'),
+        config('packages/assets', 'jsdom'),
+        config('packages/flight'),
+        config('packages/geometry'),
+        config('packages/interaction'),
+        config('packages/materials'),
+        config('packages/render/core'),
+        config('packages/scene/graph/stage'),
+        config('packages/scene/graph/world'),
+        config('packages/types'),
+        config('tests/api'),
+        config('packages/render/canvas', 'jsdom'),
+        config('tests/api/browser', 'jsdom'),
+        config('tests/integration', 'jsdom'),
+        config('tests/size'),
       ],
     },
   }),
 );
+
+function config(path: string, environment: string = 'node', groupOrder?: number): TestProjectConfiguration {
+  let name = '';
+  if (path.startsWith('packages')) {
+    name = '@flighthq/' + path.substring(path.indexOf('/') + 1).replace('/', '-');
+  } else {
+    name = path;
+  }
+  groupOrder = groupOrder ?? (environment === 'jsdom' ? 2 : 1);
+  return {
+    test: {
+      name: name,
+      dir: path,
+      sequence: { groupOrder: groupOrder },
+      environment: environment,
+    },
+    extends: resolve(path, 'vitest.config.ts'),
+  };
+}
