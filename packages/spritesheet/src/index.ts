b012d8495ced29986807394c0daeafb6454ab157
jgranick <jgranick@users.noreply.github.com>
2026-05-18 21:23:43 -0700

    spritesheet: Add spritesheetFromTileset

--- packages/spritesheet/src/index.ts
+++ packages/spritesheet/src/index.ts
@@ -1,4 +1,5 @@
 export * from './spritesheet';
 export * from './spritesheetAnimation';
 export * from './spritesheetFrame';
+export * from './spritesheetFromTileset';
 export * from './spritesheetPlayer';
---
+++ packages/spritesheet/src/spritesheetFromTileset.ts
@@ -0,0 +1,10 @@
+import type { Spritesheet, Tileset } from '@flighthq/types';
+
+import { createSpritesheet } from './spritesheet';
+import { createSpritesheetFrame } from './spritesheetFrame';
+
+export function spritesheetFromTileset(tileset: Tileset): Spritesheet {
+  const { atlas } = tileset;
+  const frames = (atlas?.regions ?? []).map((region) => createSpritesheetFrame({ id: region.id }));
+  return createSpritesheet({ atlas, frames });
+}
