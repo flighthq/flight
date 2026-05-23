7aaaf5b235bf78b3a92f4e1912c2e99339df40b2
jgranick <jgranick@users.noreply.github.com>
2026-05-23 00:27:16 -0700

    examples: AddingText

---
+++ examples/addingtext/src/app.ts
@@ -0,0 +1,45 @@
+import {
+  addChild,
+  createCanvasRenderState,
+  createDisplayObject,
+  createText,
+  defaultCanvasTextRenderer,
+  loadFontSourceFromURL,
+  registerRenderer,
+  renderCanvasBackground,
+  renderCanvasDisplayObject,
+  TextKind,
+  updateDisplayObjectBeforeRender,
+} from '@flighthq/engine';
+
+const canvas = document.createElement('canvas');
+canvas.width = 400;
+canvas.height = 200;
+document.body.appendChild(canvas);
+
+const state = createCanvasRenderState(canvas, {
+  backgroundColor: 0xffffffff,
+  contextAttributes: { alpha: false },
+});
+registerRenderer(state, TextKind, defaultCanvasTextRenderer);
+
+const font = await loadFontSourceFromURL('assets/KatamotzIkasi.woff', 'Katamotz Ikasi');
+
+const root = createDisplayObject();
+
+const textField = createText();
+textField.data.text = 'Hello World';
+textField.data.textFormat = { font: font.name, size: 30, color: 0x7a0026 };
+textField.x = 50;
+textField.y = 50;
+addChild(root, textField);
+
+function enterFrame(): void {
+  if (updateDisplayObjectBeforeRender(state, root)) {
+    renderCanvasBackground(state);
+    renderCanvasDisplayObject(state, root);
+  }
+  requestAnimationFrame(enterFrame);
+}
+
+requestAnimationFrame(enterFrame);
