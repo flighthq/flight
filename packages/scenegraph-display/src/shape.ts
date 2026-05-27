2e48ad8c5fe24180d8cf04b7fc302ae90462dd4c
Joshua Granick <jgranick@users.noreply.github.com>
2026-05-27 15:43:06 -0700

    scenegraph-display: Progress on better shape bounds

--- packages/scenegraph-display/src/shape.ts
+++ packages/scenegraph-display/src/shape.ts
@@ -17,12 +17,19 @@ export function computeShapeLocalBoundsRect(out: Rectangle, source: Readonly<Gra
   let maxX = -Infinity;
   let maxY = -Infinity;
   let strokeWidth = 0;
+  let penX = 0;
+  let penY = 0;
 
   function expand(x: number, y: number): void {
     if (x < minX) minX = x;
     if (y < minY) minY = y;
     if (x > maxX) maxX = x;
     if (y > maxY) maxY = y;
+  }
+
+  function quadPoint(t: number, p0: number, p1: number, p2: number): number {
+    const u = 1 - t;
+    return u * u * p0 + 2 * u * t * p1 + t * t * p2;
   }
 
   let i = 0;
@@ -59,20 +66,55 @@ export function computeShapeLocalBoundsRect(out: Rectangle, source: Readonly<Gra
         expand(x + w, y + h);
         break;
       }
-      case 'moveTo':
+      case 'moveTo': {
+        penX = commands[b] as number;
+        penY = commands[b + 1] as number;
+        break;
+      }
       case 'lineTo': {
-        expand(commands[b] as number, commands[b + 1] as number);
+        const x = commands[b] as number;
+        const y = commands[b + 1] as number;
+        expand(penX, penY);
+        expand(x, y);
+        penX = x;
+        penY = y;
         break;
       }
       case 'curveTo': {
-        expand(commands[b] as number, commands[b + 1] as number);
-        expand(commands[b + 2] as number, commands[b + 3] as number);
+        const controlX = commands[b] as number;
+        const controlY = commands[b + 1] as number;
+        const anchorX = commands[b + 2] as number;
+        const anchorY = commands[b + 3] as number;
+        expand(penX, penY);
+        // Expand by quadratic bezier extrema (t where derivative = 0)
+        const denomX = penX - 2 * controlX + anchorX;
+        if (denomX !== 0) {
+          const tx = (penX - controlX) / denomX;
+          if (tx > 0 && tx < 1) expand(quadPoint(tx, penX, controlX, anchorX), quadPoint(tx, penY, controlY, anchorY));
+        }
+        const denomY = penY - 2 * controlY + anchorY;
+        if (denomY !== 0) {
+          const ty = (penY - controlY) / denomY;
+          if (ty > 0 && ty < 1) expand(quadPoint(ty, penX, controlX, anchorX), quadPoint(ty, penY, controlY, anchorY));
+        }
+        expand(anchorX, anchorY);
+        penX = anchorX;
+        penY = anchorY;
         break;
       }
       case 'cubicCurveTo': {
-        expand(commands[b] as number, commands[b + 1] as number);
-        expand(commands[b + 2] as number, commands[b + 3] as number);
-        expand(commands[b + 4] as number, commands[b + 5] as number);
+        const control1X = commands[b] as number;
+        const control1Y = commands[b + 1] as number;
+        const control2X = commands[b + 2] as number;
+        const control2Y = commands[b + 3] as number;
+        const anchorX = commands[b + 4] as number;
+        const anchorY = commands[b + 5] as number;
+        expand(penX, penY);
+        expand(control1X, control1Y);
+        expand(control2X, control2Y);
+        expand(anchorX, anchorY);
+        penX = anchorX;
+        penY = anchorY;
         break;
       }
       case 'lineStyle': {
@@ -86,30 +128,44 @@ export function computeShapeLocalBoundsRect(out: Rectangle, source: Readonly<Gra
         for (const pc of pathCmds) {
           switch (pc) {
             case 1: // MOVE_TO
-              expand(data[di], data[di + 1]);
+              penX = data[di];
+              penY = data[di + 1];
               di += 2;
               break;
             case 2: // LINE_TO
+              expand(penX, penY);
               expand(data[di], data[di + 1]);
+              penX = data[di];
+              penY = data[di + 1];
               di += 2;
               break;
             case 3: // CURVE_TO
+              expand(penX, penY);
               expand(data[di], data[di + 1]);
               expand(data[di + 2], data[di + 3]);
+              penX = data[di + 2];
+              penY = data[di + 3];
               di += 4;
               break;
             case 4: // WIDE_MOVE_TO
-              expand(data[di + 2], data[di + 3]);
+              penX = data[di + 2];
+              penY = data[di + 3];
               di += 4;
               break;
             case 5: // WIDE_LINE_TO
+              expand(penX, penY);
               expand(data[di + 2], data[di + 3]);
+              penX = data[di + 2];
+              penY = data[di + 3];
               di += 4;
               break;
             case 6: // CUBIC_CURVE_TO
+              expand(penX, penY);
               expand(data[di], data[di + 1]);
               expand(data[di + 2], data[di + 3]);
               expand(data[di + 4], data[di + 5]);
+              penX = data[di + 4];
+              penY = data[di + 5];
               di += 6;
               break;
           }
