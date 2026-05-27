e9b8ea5c2ef611910f37c7fa7224031ab76a57bb
jgranick <jgranick@users.noreply.github.com>
2026-05-26 21:07:19 -0700

    assets: Improve createImageSource

--- packages/assets/src/imageSource.ts
+++ packages/assets/src/imageSource.ts
@@ -1,10 +1,15 @@
 import { createEntity } from '@flighthq/foundation';
 import type { ImageSource } from '@flighthq/types';
 
-export function createImageSource(image?: HTMLImageElement): ImageSource {
-  return createEntity({
-    height: image?.height ?? 0,
-    src: image ?? null,
-    width: image?.width ?? 0,
-  });
+export function createImageSource(image?: CanvasImageSource): ImageSource {
+  const src = image ?? null;
+  const width =
+    src instanceof HTMLVideoElement
+      ? src.videoWidth
+      : ((src as HTMLImageElement | HTMLCanvasElement | null)?.width ?? 0);
+  const height =
+    src instanceof HTMLVideoElement
+      ? src.videoHeight
+      : ((src as HTMLImageElement | HTMLCanvasElement | null)?.height ?? 0);
+  return createEntity({ height, src, width });
 }
