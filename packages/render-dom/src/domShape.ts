a0c91bc388da563857d27c8b3c2476275267f952
jgranick <jgranick@users.noreply.github.com>
2026-06-14 23:21:22 -0700

    fix(render-dom): fixes for DOM CSS filter support

--- packages/render-dom/src/domShape.ts
+++ packages/render-dom/src/domShape.ts
@@ -1,5 +1,6 @@
 ﻿import { createEntity } from '@flighthq/entity';
 import { getNodeLocalBoundsRectangle } from '@flighthq/node';
+import { hasRenderFeatures } from '@flighthq/render';
 import { renderCanvasShapeCommands } from '@flighthq/render-canvas';
 import type {
   DisplayObjectRenderer,
@@ -10,7 +11,9 @@ import type {
   RenderState,
   Shape,
 } from '@flighthq/types';
+import { RenderFeatures } from '@flighthq/types';
 
+import { getDOMCSSFilter } from './domCSSFilterBinding';
 import { prepareDOMElement, setDOMRendererElement } from './domStyle';
 import { setDOMTransformWithOffset } from './domTransform';
 
@@ -53,6 +56,9 @@ export function drawDOMShape(state: DOMRenderState, renderNode: DisplayObjectRen
   renderCanvasShapeCommands(ctx, commands);
 
   data.canvas.style.opacity = renderNode.alpha < 1 ? String(renderNode.alpha) : '';
+  if (hasRenderFeatures(state, RenderFeatures.CSSFilter)) {
+    data.canvas.style.filter = getDOMCSSFilter(renderNode) ?? '';
+  }
   state.applyBlendMode?.(data.canvas, renderNode.blendMode);
   setDOMTransformWithOffset(data.canvas, renderNode.transform2D, bounds.x, bounds.y, state.roundPixels);
 
