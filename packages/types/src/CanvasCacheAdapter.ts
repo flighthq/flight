22a64c46652e6d9ec1403e39eb0b5e8223b2c3e2
jgranick <jgranick@users.noreply.github.com>
2026-06-14 18:12:03 -0700

    feat(types)!: converge on RenderCache type

--- packages/types/src/CanvasCacheAdapter.ts
+++
@@ -1,6 +0,0 @@
-import type { CanvasCache } from './CanvasCache';
-import type { RenderCacheAdapter } from './RenderCacheAdapter';
-
-export type CanvasCacheAdapter = RenderCacheAdapter & {
-  primitive: CanvasCache | null;
-};
--- packages/types/src/ImageRenderCacheResult.ts
+++
@@ -1,7 +0,0 @@
-import type { ImageSource } from './ImageSource';
-import type { Matrix } from './Matrix';
-
-export interface ImageRenderCacheResult {
-  source: ImageSource | null;
-  transform: Matrix;
-}
