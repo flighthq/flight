c896602f45c53aca3378930e0aad207d9b59410f
Joshua Granick <joshua@joshuagranick.com>
2026-06-19 08:41:41 -0700

    feat(node): advance clipRectangle to more advanced clip region

--- packages/node/src/hasClipRectangle.ts
+++
@@ -1,5 +0,0 @@
-import type { HasClipRectangle } from '@flighthq/types';
-
-export function initClipRectangleTrait(target: HasClipRectangle, obj?: Readonly<Partial<HasClipRectangle>>): void {
-  target.clipRectangle = obj?.clipRectangle ?? null;
-}
