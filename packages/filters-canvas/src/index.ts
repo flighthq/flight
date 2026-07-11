b48ca2a9146895cb7fd460294830c4de0db1ace2
Joshua Granick <jgranick@users.noreply.github.com>
2026-07-10 22:39:34 -0700

    refactor(filters): remove dead fused list-appliers with no consumers

--- packages/filters-canvas/src/index.ts
+++ packages/filters-canvas/src/index.ts
@@ -1,4 +1,3 @@
 export { applyBlurFilterToCanvas } from './canvasBlurFilter';
 export { applyDropShadowFilterToCanvas } from './canvasDropShadowFilter';
-export { applyCanvasFilter, canUseCanvasFilterCssFor } from './canvasFilterDispatch';
 export { applyOuterGlowFilterToCanvas } from './canvasOuterGlowFilter';
