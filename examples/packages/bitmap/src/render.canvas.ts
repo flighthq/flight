ddb97eb6980a7f9efcd5e0345644fe649e65538b
Joshua Granick <jgranick@users.noreply.github.com>
2026-07-14 09:33:34 -0700

    feat(example): add bitmap example with procedural images

---
+++ examples/packages/bitmap/src/render.canvas.ts
@@ -0,0 +1,30 @@
+import type { DisplayObject } from '@flighthq/sdk';
+import {
+  BitmapKind,
+  createCanvasElement,
+  createCanvasRenderState,
+  defaultCanvasBitmapRenderer,
+  prepareDisplayObjectRender,
+  registerRenderer,
+  renderCanvasBackground,
+  renderCanvasDisplayObject,
+} from '@flighthq/sdk';
+
+const pixelRatio = window.devicePixelRatio || 1;
+const canvas = createCanvasElement(800, 500, pixelRatio);
+document.body.appendChild(canvas);
+
+export const state = createCanvasRenderState(canvas, {
+  sceneGraphSyncPolicy: 'requiresInvalidation',
+  backgroundColor: 0xf0f0f0ff,
+});
+
+registerRenderer(state, BitmapKind, defaultCanvasBitmapRenderer);
+
+export const scale = pixelRatio;
+
+export function render(root: DisplayObject): void {
+  if (!prepareDisplayObjectRender(state, root)) return;
+  renderCanvasBackground(state);
+  renderCanvasDisplayObject(state, root);
+}
