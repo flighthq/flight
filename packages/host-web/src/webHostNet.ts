31c77cd6f21a2c9fff9d6d18b0ff0bc767047ec8
Joshua Granick <jgranick@users.noreply.github.com>
2026-08-30 15:19:26 -0700

    feat(host-web): use createHost with http+socket in webHostNet

--- packages/host-web/src/webHostNet.ts
+++ packages/host-web/src/webHostNet.ts
@@ -1,8 +1,8 @@
-import { createEntity } from '@flighthq/entity/contract';
-import type { EntityRuntimeKey, HasNetHttp } from '@flighthq/types/contract';
+import { createHost } from '@flighthq/entity/contract';
 
 import { webNetBackend } from './webNet';
+import { webSocketBackend } from './webSocket';
 
-export const webHostNet = createEntity({
-  net: { http: webNetBackend },
-} as const satisfies Omit<HasNetHttp, typeof EntityRuntimeKey>);
+export const webHostNet = createHost({
+  net: { http: webNetBackend, socket: webSocketBackend },
+});
