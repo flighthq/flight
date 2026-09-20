01db782da78c6768eebf4192e7ac3802a7892d31
Joshua Granick <jgranick@users.noreply.github.com>
2026-09-20 03:57:10 -0700

    style(exports): format the regenerated lane barrels

--- packages/scene2d-formats/src/index.ts
+++ packages/scene2d-formats/src/index.ts
@@ -1,60 +1,24 @@
-export {
-  applyAnimationClipToLottieDocument,
-  createScene2DFromLottieDocument,
-} from './lottieDocument';
+export { applyAnimationClipToLottieDocument, createScene2DFromLottieDocument } from './lottieDocument';
 export * from './riveAnimation';
 export * from './riveAssetBinding';
-export {
-  registerRiveAssetHandlers,
-} from './riveAssets';
-export {
-  registerRiveClippingHandlers,
-} from './riveClipping';
+export { registerRiveAssetHandlers } from './riveAssets';
+export { registerRiveClippingHandlers } from './riveClipping';
 export * from './riveCoreProperties';
 export * from './riveCoreTypes';
 export * from './riveDocument';
-export {
-  registerRiveDrawOrderHandlers,
-} from './riveDrawOrder';
+export { registerRiveDrawOrderHandlers } from './riveDrawOrder';
 export * from './riveHandlers';
-export {
-  createRiveImportRegistry,
-  registerRiveCoreObjectHandler,
-} from './riveImportRegistry';
-export {
-  registerRiveLayoutHandlers,
-} from './riveLayout';
-export {
-  createRiveObjectGraph,
-} from './riveObjectGraph';
-export {
-  createRiveDocumentImportResult,
-  createScene2DFromRiveDocument,
-} from './riveScene2D';
-export {
-  createScene2DDocumentFromRiveDocument,
-} from './riveScene2DDocument';
-export {
-  registerRiveShapeHandlers,
-} from './riveShapeNode';
+export { createRiveImportRegistry, registerRiveCoreObjectHandler } from './riveImportRegistry';
+export { registerRiveLayoutHandlers } from './riveLayout';
+export { createRiveObjectGraph } from './riveObjectGraph';
+export { createRiveDocumentImportResult, createScene2DFromRiveDocument } from './riveScene2D';
+export { createScene2DDocumentFromRiveDocument } from './riveScene2DDocument';
+export { registerRiveShapeHandlers } from './riveShapeNode';
 export * from './riveShapePaint';
-export {
-  createRivePath,
-  createRivePathRecord,
-  registerRivePathHandlers,
-} from './riveShapePath';
-export {
-  registerRiveSkeletonHandlers,
-} from './riveSkeleton';
+export { createRivePath, createRivePathRecord, registerRivePathHandlers } from './riveShapePath';
+export { registerRiveSkeletonHandlers } from './riveSkeleton';
 export * from './riveSkin';
-export {
-  registerRiveSoloHandlers,
-} from './riveSolo';
-export {
-  registerRiveStateMachineHandlers,
-} from './riveStateMachine';
-export {
-  createRiveRichText,
-  registerRiveTextHandlers,
-} from './riveText';
+export { registerRiveSoloHandlers } from './riveSolo';
+export { registerRiveStateMachineHandlers } from './riveStateMachine';
+export { createRiveRichText, registerRiveTextHandlers } from './riveText';
 export * from './svgDocument';
