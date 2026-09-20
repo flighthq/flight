c14f1c00db22e10c51c2e5d302cc39d72699eeff
Joshua Granick <jgranick@users.noreply.github.com>
2026-09-20 02:05:21 -0700

    fix(host-electron): keep populate helpers and test support off both export lanes

--- packages/host-electron/src/contract.ts
+++ packages/host-electron/src/contract.ts
@@ -1,19 +1,129 @@
-export * from './electronApp';
+export {
+  electronHostApp,
+  electronHostAppActivate,
+  electronHostAppActivationPolicy,
+  electronHostAppAllWindowsClosed,
+  electronHostAppBadge,
+  electronHostAppDock,
+  electronHostAppFocus,
+  electronHostAppHide,
+  electronHostAppLocale,
+  electronHostAppLoginItem,
+  electronHostAppName,
+  electronHostAppNameWrite,
+  electronHostAppOpenFile,
+  electronHostAppPath,
+  electronHostAppQuit,
+  electronHostAppQuitRequest,
+  electronHostAppReady,
+  electronHostAppRecentDocuments,
+  electronHostAppRelaunch,
+  electronHostAppSecondInstance,
+  electronHostAppShow,
+  electronHostAppSingleInstance,
+  electronHostAppUserModelId,
+  electronHostAppVersion,
+} from './electronApp';
 export * from './electronClipboard';
 export * from './electronDefaultHostGroups';
-export * from './electronDialog';
-export * from './electronIpc';
-export * from './electronMenu';
-export * from './electronMenuTemplate';
-export * from './electronNotification';
-export * from './electronPlatform';
-export * from './electronPower';
-export * from './electronProtocol';
+export {
+  electronHostDialog,
+  electronHostDirectoryOpenDialog,
+  electronHostFileOpenDialog,
+  electronHostFileSaveDialog,
+  electronHostMessageDialog,
+} from './electronDialog';
+export {
+  electronHostIpc,
+  electronHostIpcHandle,
+  electronHostIpcInvoke,
+  electronHostIpcMessage,
+  electronHostIpcSend,
+  electronHostIpcTargetedSend,
+} from './electronIpc';
+export { electronHostAppMenu, electronHostMenu, electronHostMenuPopup, electronHostMenuSelect } from './electronMenu';
+export {
+  electronHostNotification,
+  electronHostNotificationAction,
+  electronHostNotificationClick,
+  electronHostNotificationClose,
+  electronHostNotificationDelivery,
+  electronHostNotificationDismiss,
+  electronHostNotificationLifecycle,
+  electronHostNotificationReceived,
+  electronHostNotificationReply,
+} from './electronNotification';
+export { electronHostPlatform, electronHostPlatformGroup } from './electronPlatform';
+export {
+  electronHostPower,
+  electronHostPowerBatteryHealth,
+  electronHostPowerChange,
+  electronHostPowerIdle,
+  electronHostPowerKeepAwake,
+  electronHostPowerSessionLock,
+  electronHostPowerStatus,
+  electronHostPowerSuspension,
+  electronHostPowerThermal,
+} from './electronPower';
+export {
+  electronHostProtocol,
+  electronHostProtocolDefault,
+  electronHostProtocolOpen,
+  electronHostProtocolRegistration,
+  electronHostProtocolRegistrationQuery,
+  electronHostProtocolUnregistration,
+} from './electronProtocol';
 export * from './electronRegister';
-export * from './electronScreen';
-export * from './electronShell';
-export * from './electronShortcut';
-export * from './electronStorage';
-export * from './electronTray';
-export * from './electronUpdater';
-export * from './electronWindow';
+export { electronHostScreen, electronHostScreenChange, electronHostScreenQuery } from './electronScreen';
+export {
+  electronHostShell,
+  electronHostShellBeep,
+  electronHostShellExternal,
+  electronHostShellPathOpen,
+  electronHostShellPathReveal,
+  electronHostShellShortcutLink,
+  electronHostShellTrash,
+} from './electronShell';
+export { electronHostShortcut, electronHostShortcutQuery, electronHostShortcutTrigger } from './electronShortcut';
+export { electronHostStorage, electronHostStorageGroup } from './electronStorage';
+export {
+  electronHostTray,
+  electronHostTrayBalloon,
+  electronHostTrayBalloonEvents,
+  electronHostTrayBounds,
+  electronHostTrayDoubleClickPolicy,
+  electronHostTrayDropEvents,
+  electronHostTrayImage,
+  electronHostTrayInteractionEvents,
+  electronHostTrayLifecycle,
+  electronHostTrayMenu,
+  electronHostTrayMenuSelectionEvents,
+  electronHostTrayPopupMenu,
+  electronHostTrayPressedImage,
+  electronHostTrayTemplateImage,
+  electronHostTrayTitle,
+  electronHostTrayTooltip,
+} from './electronTray';
+export { electronHostUpdater, electronHostUpdaterCommand } from './electronUpdater';
+export {
+  electronHostWindow,
+  electronHostWindowAppearance,
+  electronHostWindowAttach,
+  electronHostWindowAttention,
+  electronHostWindowContentProtection,
+  electronHostWindowFocus,
+  electronHostWindowFullscreen,
+  electronHostWindowGeometry,
+  electronHostWindowHierarchy,
+  electronHostWindowLifecycle,
+  electronHostWindowProgress,
+  electronHostWindowShadow,
+  electronHostWindowShell,
+  electronHostWindowSizeConstraints,
+  electronHostWindowState,
+  electronHostWindowVisibility,
+  electronHostWindowZOrder,
+  getAppWindowForElectronId,
+  getElectronBrowserWindow,
+  getElectronWindowId,
+} from './electronWindow';
